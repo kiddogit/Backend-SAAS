81c0bf31e6e7e6f519d3d47125bbf4957f13cc7a
kiddogit <85332892+kiddogit@users.noreply.github.com>
2025-05-01 11:19:28 +0545

    file nameing of models and connect into db

--- database/connection.js
+++ database/connection.js
@@ -3,6 +3,8 @@
 
 const {Sequelize, DataTypes} = require("sequelize")
 
+// const bookModel = require("./models/book.model")
+
 const sequelize = new Sequelize("postgresql://postgres.mdxdtwmargzjipcdcdmt:rajbanshialka@aws-0-ap-south-1.pooler.supabase.com:6543/postgres?pgbouncer=true")
 
 sequelize.authenticate()
@@ -16,5 +18,14 @@ sequelize.authenticate()
 const db = {}
 db.Sequelize = Sequelize
 db.sequelize = sequelize
+
+db.books = require("./models/book.model")(sequelize, DataTypes)
+db.users = require("./models/user.model")(sequelize, DataTypes)
+db.products = require("./models/product.model")(sequelize, DataTypes)
+
+// migrate code ho yo 
+sequelize.sync({alter : false}).then(() => {
+  console.log("Migrate hit vayo hai tw")
+})
 
 module.exports = db
